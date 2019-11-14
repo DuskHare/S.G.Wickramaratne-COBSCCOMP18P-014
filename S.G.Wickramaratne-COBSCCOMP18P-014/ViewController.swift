f1ebb371718144b109a2da514ca5daaf0a9d7cae
Imesh Gunasekara <imeshpro90@gmail.com>
2019-11-14 19:02:42 +0530

    Initial Commit

---
+++ S.G.Wickramaratne-COBSCCOMP18P-014/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  S.G.Wickramaratne-COBSCCOMP18P-014
+//
+//  Created by Digital-03 on 11/14/19.
+//  Copyright © 2019 Shaveen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
